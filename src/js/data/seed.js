1a2dc463a8ebdf03ea8d595b056745fe2a5a1d85
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-01 21:20:36 +1000

    create temporary database

---
+++ src/js/data/seed.js
@@ -0,0 +1,31 @@
+const restaurants = [
+  {
+    id: 1,
+    name: 'ByKorea',
+    adWord: 'Korean ginseng chicken soup',
+    minTemperature: 0,
+    maxTemperature: 10,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 2,
+    name: 'Juicy Bao',
+    adWord: 'juicy Xiaolongbao',
+    minTemperature: 10,
+    maxTemperature: 20,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 3,
+    name: 'Bornga',
+    adWord: 'cold noodle',
+    minTemperature: 20,
+    maxTemperature: 40,
+    foodPic: 'pic',
+    location: 'url'
+  }
+];
+
+export { restaurants };
