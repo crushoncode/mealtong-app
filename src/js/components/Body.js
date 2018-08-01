587b82638bb4c17bcdb45b409f4307188563149c
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-01 21:28:55 +1000

    add slidecontainer with handleChange

--- src/js/components/Body.js
+++ src/js/components/Body.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import { SetTemp } from './SetTemp';
+import { Deck } from './Deck';
 
 // before making functions in SetTemp component,
 // set state and make function in Body component first.
-class Body extends React.Component {
-    state = {
-        temperature: 0
-    }
 
-    changeTemp = (temp) => {
-        this.setState(() => {
-            return {
-                temperature: temp
-            }
-        });
-    }
-  
-    render() {
-        return (
-            <div>
-                <SetTemp changeTemp={this.changeTemp} />
-                <p> temperature {this.state.temperature} </p>
-            </div>
-        )
-    }
+class Body extends React.Component {
+  state = {
+    temperature: 0
+  };
+
+  changeTemp = (temp) => {
+    this.setState(() => {
+      return {
+        temperature: temp
+      };
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  // temperature {this.state.value} is passed into Deck temperature={this.state value}
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="40"
+            defaultValue="20"
+            value={this.state.value}
+            className="slider"
+            onChange={this.handleChange}
+            id="myRange"
+          />
+          <SetTemp changeTemp={this.changeTemp} />
+          <p> temperature={this.state.value} </p>
+        </div>
+
+        <div>
+          <Deck temperature={this.state.value} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export { Body }
+export { Body };
