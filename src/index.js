e787aca6fdf632d2e08f72bf1dba1c4e19d661cf
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-02 19:34:40 +1000

    change file from scss to css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import './css/styles.scss';
+import './css/styles.css';
 import MealtongApp from './js/components/MealtongApp';
 
-
 ReactDOM.render(<MealtongApp />, document.getElementById('root'));
-
-
