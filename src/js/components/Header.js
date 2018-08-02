bc000f7f72306f417ed160aeda0f26a65f1fbcf6
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-02 19:30:46 +1000

    change header-subtitle content

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Header = () => {
-    return (
-        <div className="header">
-            <h3 className="header_title">M E A L T O N G</h3>
-            <p className="header_subtitle">Try a new way to find what to eat and where to go! <br/>
-            Ready? Drag to the current temperature and press “SHUFFLE MEALTONG” button!</p> 
-        </div> 
-    )
-}
+  return (
+    <div className="header">
+      <h3 className="header-title">M E A L T O N G</h3>
+      <p className="header-subtitle">
+        Still not sure about what to eat and where to go? <br />
+        Simply drag to the current temperature and enjoy Serina's
+        recommendations :)
+      </p>
+    </div>
+  );
+};
 
-export { Header }
+export { Header };
