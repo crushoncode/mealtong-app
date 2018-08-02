0bc4b4893e1c7974d80bbe8f7d2262635ebc515e
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-02 22:32:55 +1000

    fix error

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header">
       <h3 className="header-title">M E A L T O N G</h3>
       <p className="header-subtitle">
-        Still not sure about what to eat and where to go? <br />
+        Still not sure of what to eat and where to go? <br />
         Simply drag to the current temperature and enjoy Serina's
         recommendations :)
       </p>
