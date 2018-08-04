a89ca851ccd3795a037baa83eea680d679e1f84b
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-04 22:48:49 +1000

    make more data in seed

--- src/js/data/seed.js
+++ src/js/data/seed.js
@@ -10,6 +10,24 @@ const restaurants = [
   },
   {
     id: 2,
+    name: 'Sooltong',
+    adWord: 'Korean ginseng chicken soup',
+    minTemperature: 0,
+    maxTemperature: 10,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 3,
+    name: 'Korean table',
+    adWord: 'Korean ginseng chicken soup',
+    minTemperature: 0,
+    maxTemperature: 10,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 4,
     name: 'Juicy Bao',
     adWord: 'juicy Xiaolongbao',
     minTemperature: 10,
@@ -18,8 +36,44 @@ const restaurants = [
     location: 'url'
   },
   {
-    id: 3,
+    id: 5,
+    name: 'Amugeona',
+    adWord: 'juicy Xiaolongbao',
+    minTemperature: 10,
+    maxTemperature: 20,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 6,
+    name: 'Node',
+    adWord: 'juicy Xiaolongbao',
+    minTemperature: 10,
+    maxTemperature: 20,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 7,
     name: 'Bornga',
+    adWord: 'cold noodle',
+    minTemperature: 20,
+    maxTemperature: 40,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 8,
+    name: 'Java',
+    adWord: 'cold noodle',
+    minTemperature: 20,
+    maxTemperature: 40,
+    foodPic: 'pic',
+    location: 'url'
+  },
+  {
+    id: 9,
+    name: 'Python',
     adWord: 'cold noodle',
     minTemperature: 20,
     maxTemperature: 40,
