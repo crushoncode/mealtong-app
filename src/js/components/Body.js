e0b87ed83d45e7dfd998837a2c99dbfb37542d79
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-04 22:49:52 +1000

    separate slider part from body

--- src/js/components/Body.js
+++ src/js/components/Body.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import { Deck } from './Deck';
-
-// before making functions in SetTemp component,
-// set state and make function in Body component first.
+import { Slider } from './Slider';
 
 class Body extends React.Component {
   state = {
-    temperature: 0,
-    maxValue: 100, // the higher the smoother when dragging
-    speed: 5
+    temperature: null
   };
 
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
+  handleLike = (restaurant) => {
+    this.props.handleLike(restaurant);
+  };
+
+  handleTemperatureChange = (temperature) => {
+    this.setState({
+      temperature: temperature
+    });
   };
 
   render() {
     return (
       <React.Fragment>
-        <input
-          type="range"
-          min="0"
-          max="40"
-          defaultValue="20"
-          value={this.state.value}
-          className="range"
-          onChange={this.handleChange}
-          id="myRange"
-        />
         <p className="temp-indicator">
-          The current temperature is {this.state.value}ºC.
+          The current temperature is {this.state.temperature}ºC.
         </p>
-        <Deck temperature={this.state.value} />
+        <Slider onChange={this.handleTemperatureChange} />
+        <Deck
+          temperature={this.state.temperature}
+          handleLike={this.handleLike}
+        />
       </React.Fragment>
     );
   }
