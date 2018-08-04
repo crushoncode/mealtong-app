63bb023a2d75971998fae1d91a864ddb7c7890b6
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-04 22:50:25 +1000

    add react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './css/styles.css';
 import MealtongApp from './js/components/MealtongApp';
 
-ReactDOM.render(<MealtongApp />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <MealtongApp />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
