d38aee96438700d0d36cd658ff8aaa6f5629d5fc
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-04 22:51:02 +1000

    make handlelike function

--- src/js/components/Deck.js
+++ src/js/components/Deck.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { restaurants } from '../data/seed';
 
 class Deck extends React.Component {
+  handleLike = (restaurant) => {
+    this.props.handleLike(restaurant);
+  };
+
   render() {
+    if (this.props.temperature == null) {
+      return null;
+    }
+
     // search which restaurant matches the criteria
     // check if the selected temperature is between minTemp and maxTemp
-    const matchingRestaurants = restaurants.filter((restaurant) => {
+    let matchingRestaurants = restaurants.filter((restaurant) => {
       if (
         this.props.temperature >= restaurant.minTemperature &&
         this.props.temperature <= restaurant.maxTemperature
@@ -15,17 +23,21 @@ class Deck extends React.Component {
       return false;
     });
 
+    matchingRestaurants = matchingRestaurants.slice(0, 2);
+
     // map over matchingRestaurants
     const listItems = matchingRestaurants.map((restaurant) => {
       return (
         <div className="card-container">
           <div className="card">
             <div className="restaurant-name" key={restaurant.id}>
-              {restaurant.name}
-            </div>
-          </div>
-          <div className="card">
-            <div className="restaurant-name" key={restaurant.id}>
+              <button
+                onClick={() => {
+                  this.handleLike(restaurant);
+                }}
+              >
+                like
+              </button>
               {restaurant.name}
             </div>
           </div>
