e050b424d08e188f4c43b00fdd9d2a2b21c69c92
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-08-04 22:51:51 +1000

    save restaurant data in savelist

--- src/js/components/MealtongApp.js
+++ src/js/components/MealtongApp.js
@@ -1,14 +1,113 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Navbar } from './core/Navbar';
 import { Header } from './Header';
 import { Body } from './Body';
+import { Login } from './auth/Login';
+import { Register } from './auth/Register';
+import { Savelist } from './Savelist';
+import { Review } from './Review';
+import { Contact } from './Contact';
 
-const MealtongApp = () => {
-  return (
-    <div>
-      <Header />
-      <Body />
-    </div>
-  );
-};
+class MealtongApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      savelist: [],
+      loaded: false,
+      loggedIn: false,
+      name: null,
+      email: null,
+      authToken: null,
+      redirectHome: false
+    };
+  }
+
+  handleLike = (restaurant) => {
+    this.setState(
+      () => ({
+        savelist: [...this.state.savelist, restaurant]
+      }),
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          loggedIn={this.state.loggedIn}
+          handleLogout={this.handleLogout}
+        />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <Header />
+                <Body
+                  loggedIn={this.state.loggedIn}
+                  handleLogin={this.handleLogin}
+                  handleRegister={this.handleRegister}
+                  handleLike={this.handleLike}
+                />
+              </React.Fragment>
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={() => (
+              <Login
+                loggedIn={this.state.loggedIn}
+                handleLogin={this.handleLogin}
+                handleRegister={this.handleRegister}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/register"
+            render={() => (
+              <Register
+                loggedIn={this.state.loggedIn}
+                handleLogin={this.handleLogin}
+                handleRegister={this.handleRegister}
+              />
+            )}
+          />
+          <Route
+            path="/savelist"
+            render={() => <Savelist savelist={this.state.savelist} />}
+          />
+          <Route
+            path="/review"
+            render={() =>
+              this.state.loggedIn ? <Review /> : <Redirect to="/" />
+            }
+          />
+          <Route
+            path="/contact"
+            render={() =>
+              this.state.loggedIn ? <Contact /> : <Redirect to="/" />
+            }
+          />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+// const MealtongApp = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Body />
+//     </div>
+//   );
+// };
 
 export default MealtongApp;
